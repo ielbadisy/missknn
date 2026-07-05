5ba89dd1c74dd8e26d5958ae8c80b1cb5ea91fed
ielbadisy <elbadisyimad@gmail.com>
2026-07-05 19:24:32 +0100

    Add unit tests for single and multiple imputation

---
+++ tests/testthat/test-missknn.R
@@ -0,0 +1,15 @@
+test_that("single imputation returns a completed object", {
+  x <- data.frame(a = c(1, NA, 3), b = c(2, 4, NA))
+  imp <- missknn::missknn(x, k = 1)
+  out <- missknn::complete(imp)
+  expect_true(is.data.frame(out) || data.table::is.data.table(out))
+  expect_false(anyNA(out))
+})
+
+test_that("multiple imputation returns a list", {
+  x <- data.frame(a = c(1, NA, 3), b = c(2, 4, NA))
+  imp <- missknn::missknn(x, k = 1, m = 2, seed = 1)
+  out <- missknn::complete(imp)
+  expect_type(out, "list")
+  expect_equal(length(out), 2L)
+})
