47c784bd4dfb876160b3684bc5834fd133aee1a5
ielbadisy <elbadisyimad@gmail.com>
2026-07-05 23:17:35 +0100

    Pin parallel_cores in the multiple-imputation test for CRAN check environments

--- tests/testthat/test-missknn.R
+++ tests/testthat/test-missknn.R
@@ -8,7 +8,7 @@ test_that("single imputation returns a completed object", {
 
 test_that("multiple imputation returns a list", {
   x <- data.frame(a = c(1, NA, 3), b = c(2, 4, NA))
-  imp <- missknn::missknn(x, k = 1, m = 2, seed = 1)
+  imp <- missknn::missknn(x, k = 1, m = 2, seed = 1, parallel_cores = 1L)
   out <- missknn::complete(imp)
   expect_type(out, "list")
   expect_equal(length(out), 2L)
